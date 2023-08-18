e7da4eb30ef365d9d73032d0a4e34f6a87b579ef
fizzym_VBox <miti@example.com>
2023-08-18 12:01:51 -0700

    [WIP] Implement clue generation and recording * Plates generation working - needs to be completed * Clue recording in progress

--- enph353/enph353_gazebo/scripts/plate_generator.py
+++ enph353/enph353_gazebo/scripts/plate_generator.py
@@ -11,9 +11,72 @@ import string
 from random import randint
 from PIL import Image, ImageFont, ImageDraw
 
+'''
+* 1. Criminal (10pts): Einstein, Eintoine, DR DRE, YODA, Shrek, Luigi, Mario
+* 2. Weapons (5 pts): a fishing rod, rocket, antimatter,
+* 3. Location (5 pts): hospital, mall, forest
+* 4. Motive: gluttony, curiosity, 
+* 5. DateTime: dawn, dusk, midnight, midday,
+* 6. Crime: steal, trespass, lie to
+* 7. Victim: parrots, robots, people, dogs, jedi, 
+* 8. Victims number: 5, 10, 100
+'''
+
+entries = {'criminals': ["EINSTEIN", "YODA", "SHREK", "LUIGI"],
+           'weapons': ["A FISHING ROD", "ROCKET", "ANTIMATTER"],
+           'location': ["HOSPITAL", "MALL", "FOREST", "MOON"],
+           'motive': ["GLUTTONY", "CURIOSITY", "IGNORANCE"]}
+
+# Find the path to this script
 path = os.path.dirname(os.path.realpath(__file__)) + "/"
 texture_path = '../media/materials/textures/'
 
+# write plates to plates.csv
+with open(path + "plates.csv", 'w') as plates_file:
+    csvwriter = csv.writer(plates_file)
+
+    i = 0
+    for key in entries:
+        # pick a random criminal
+        j = random.randint(0, len(entries[key]))
+        plate = entries[key][j]
+
+        plate_str = plate
+        print(plate_str)
+        csvwriter.writerow([plate_str])
+
+        # Generate plate
+        blank_plate = cv2.imread(path+'blank_plate.png')
+
+        # To use monospaced font for the license plate we need to use the PIL
+        # package.
+        # Convert into a PIL image (this is so we can use the monospaced fonts)
+        blank_plate_pil = Image.fromarray(blank_plate)
+        # Get a drawing context
+        draw = ImageDraw.Draw(blank_plate_pil)
+        monospace = ImageFont.truetype("/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf", 200)
+        draw.text((48, 105),plate_str, (255,0,0), font=monospace)
+        # Convert back to OpenCV image and save
+        blank_plate = np.array(blank_plate_pil)
+
+        # Create parking spot label
+        s = key
+        parking_spot = 255 * np.ones(shape=[600, 600, 3], dtype=np.uint8)
+        parking_spot_pil = Image.fromarray(parking_spot)
+        draw = ImageDraw.Draw(parking_spot_pil)
+        monospace = ImageFont.truetype("/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf", 200)
+        draw.text((48, 105), s, (255,0,0), font=monospace)
+        parking_spot = np.array(parking_spot_pil)
+        spot_w_plate = np.concatenate((parking_spot, blank_plate), axis=0)
+
+         # Merge images and save
+        unlabelled = np.concatenate((255 * np.ones(shape=[600, 600, 3],
+                                    dtype=np.uint8), spot_w_plate), axis=0)
+        cv2.imwrite(os.path.join(path+texture_path+"unlabelled/",
+                                 "plate_" + str(i) + ".png"), unlabelled)
+        i += 1
+
+'''
 with open(path + "plates.csv", 'w') as plates_file:
     csvwriter = csv.writer(plates_file)
 
@@ -72,3 +135,4 @@ with open(path + "plates.csv", 'w') as plates_file:
                                  "plate_" + str(i) + ".png"), labelled)
         cv2.imwrite(os.path.join(path+texture_path+"unlabelled/",
                                  "plate_" + str(i) + ".png"), unlabelled)
+'''
